dbd53e5f0b7b47e8799484b280c5b5541a8057eb
Patryk_Swierczek <patryk.swierczek@student.wsb.edu.pl>
2023-06-08 16:07:03 +0200

    obliczenia

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -1,11 +1,26 @@
 namespace Testy_jednostkowe
 {
-    public class UnitTest1
+    public class Rownanie_Kwadratowe_Test
     {
-        [Fact]
-        public void Test1()
+        string Obliczenia(double a, double b, double c)
         {
+            double dlt = b * b - 4 * a * c;
 
+            if (dlt > 0)
+            {
+                double x1 = (-b + Math.Sqrt(dlt)) / (2 * a);
+                double x2 = (-b - Math.Sqrt(dlt)) / (2 * a);
+                return $"Równanie ma dwa pierwiastki rzeczywiste:\nx1 = {x1}\nx2 = {x2}";
+            }
+            else if (dlt == 0)
+            {
+                double x = -b / (2 * a);
+                return $"Równanie ma jeden pierwiastek rzeczywisty:\nx = {x}";
+            }
+            else
+            {
+                return "Równanie nie ma pierwiastków rzeczywistych.";
+            }
         }
     }
 }
